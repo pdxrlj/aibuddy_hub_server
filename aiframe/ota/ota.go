c034b312800bf49d531ba003522256f5dbae950e
junpdx@gmail.com <junpdx@gmail.com>
2026-02-25 21:18:35 +0800

    代码优化

--- aiframe/ota/ota.go
+++ aiframe/ota/ota.go
@@ -1,7 +1,9 @@
 // Package ota 提供 OTA 固件升级相关功能
 package ota
 
-// "type": "ota_version",
+import "encoding/json"
+
+// 		"type": "ota_version",
 //     "ota_url": "https://example.com/firmware/v2.0.0.bin",  // 升级包下载地址
 //     "model_url":"xxxxx",
 //     "ota_version": "2.0.0",        // 升级包版本号
@@ -20,4 +22,12 @@ type Ota struct {
 	ForceUpdateModel int    `json:"force_update_model"`
 	UpdateContent    string `json:"update_content"`
 	DeviceID         string `json:"device_id"`
+}
+
+func (o *Ota) Encode() ([]byte, error) {
+	return json.Marshal(o)
+}
+
+func (o *Ota) Decode(data []byte) error {
+	return json.Unmarshal(data, o)
 }
